7622ad7f24ed3ae6fed3050d8358031ab1db4658
ANRoebuck <ANRoebuck@googlemail.com>
2019-06-24 16:20:06 +0100

    app setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const { apiRouter } = require("./routers/apiRouter");
+
+
+app.use(express.json());
+
+app.use('/api', apiRouter);
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { app };
---
+++ routers/apiRouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const apiRouter = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { apiRouter };
