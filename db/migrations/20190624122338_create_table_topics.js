0697ec9de46c1449cfcf8a7e633e30c1003f3006
ANRoebuck <ANRoebuck@googlemail.com>
2019-06-24 12:30:50 +0100

    migrates made for topics and users

--- db/migrations/20190624122338_create_table_topics.js
+++ db/migrations/20190624122338_create_table_topics.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('topics', topicsTable => {
+      topicsTable.string('slug').primary();
+      topicsTable.sting.('description').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('topics');
 };
